77bf2b8480ffc3c9a2f74e519f264b09f3bd6028
Versina Vasile <ipvvg4@gmail.com>
2024-02-07 13:17:47 +0100

    add comment in MainMenu

--- src/CurrencyExchange/Menu/MainMenu.java
+++ src/CurrencyExchange/Menu/MainMenu.java
@@ -2,13 +2,13 @@ package CurrencyExchange.Menu;
 
 import CurrencyExchange.Exchange.ExchangeHistory;
 
-public class MainMenu {
+public class MainMenu { // Основное меню программы
 
   public static void main(String[] args) {
-    MainApp mainMenu = new MainApp();
-    mainMenu.runMainMenu();
-    ExchangeHistory exchangeHistory = new ExchangeHistory();
-    mainMenu.display();
-    exchangeHistory.viewHistory();
+    MainApp mainMenu = new MainApp(); // Создание объекта для основного меню
+    mainMenu.runMainMenu();          // Запуск главного меню
+    ExchangeHistory exchangeHistory = new ExchangeHistory();  // Создание объекта для истории обмена валюты
+    mainMenu.display(); // Отображение главного меню
+    exchangeHistory.viewHistory(); // Просмотр истории обмена валюты
   }
 }
